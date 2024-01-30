f1491601f9a42e4d86992e51d290f756548d46ba
Kelly Schumacher <kschumacher93@gmail.com>
2024-01-29 22:57:47 -0500

    added styles and starter template

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      {/* <Avatar /> */}
+      <div className="data">
+        {/* <Intro /> */}
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        {/* <SkillList /> */}
+      </div>
     </div>
   );
 }
